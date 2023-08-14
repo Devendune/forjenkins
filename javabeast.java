0f5eea0c5cd9ae41ae526fad04cddee515dbf711
Devendune <80749077+Devendune@users.noreply.github.com>
2023-08-14 12:28:34 +0530

    Update javabeast.java

--- javabeast.java
+++ javabeast.java
@@ -3,5 +3,7 @@ class javabeast{
     public static void main(String [] args)
     {
         System.out.println("hi");
+        System.out.println("we are in the world of greatness");
+        System.out.println("this world is created by Devendu");
     }
 }
